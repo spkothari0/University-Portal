501139707d06c0d1a4e831a74783644a380d1971
Damini Thorat <thorat.d@northeastern.edu>
2024-04-19 20:24:11 -0400

    Added organization and roles

--- HuskyHub/src/Business/Organization/ServiceWorkerOrganization.java
+++ HuskyHub/src/Business/Organization/ServiceWorkerOrganization.java
@@ -4,25 +4,28 @@
  */
 package Business.Organization;
 
-import Business.Role.AdminRole;
 import Business.Role.Role;
+import Business.Role.ServiceWorkerRole;
 import java.util.ArrayList;
 
 /**
  *
  * @author omkar
  */
-public class AdminOrganization extends Organization{
+public class ServiceWorkerOrganization extends Organization{
 
-    public AdminOrganization() {
-        super(Type.Admin.getValue());
+    public ServiceWorkerOrganization() {
+        super(Organization.Type.ServiceWorker.getValue());
     }
-    
+
     @Override
     public ArrayList<Role> getSupportedRole() {
         ArrayList<Role> roles = new ArrayList<>();
-        roles.add(new AdminRole());
+        roles.add(new ServiceWorkerRole());
         return roles;
     }
      
+   
+    
+    
 }
