e8a90180640cb58317bf40ded16a88efd10374ea
khavnekar-y <khavnekar.y@northeastern.edu>
2024-04-19 20:44:48 -0400

    Created Student Java Classes

--- HuskyHub/src/Business/Student/StudentDirectory.java
+++ HuskyHub/src/Business/Student/StudentDirectory.java
@@ -1,27 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package Business.Student;
 
 import java.util.ArrayList;
 
+/**
+ *
+ * @author yash
+ */
 public class StudentDirectory {
+    
     private ArrayList<Student> studentList;
 
     public StudentDirectory() {
-        studentList = new ArrayList();
+        studentList = new ArrayList<>();
     }
 
     public ArrayList<Student> getStudentList() {
         return studentList;
     }
-
-    public void setStudentList(ArrayList<Student> studentList) {
-        this.studentList = studentList;
-    }
-
-    public Student createNewStudent(String name) {
-        Student student=null;
-        student = name == null ? new Student() : new Student(name);
-        student.setName(name);
+    
+    public Student createStudent(Student student){
         studentList.add(student);
         return student;
     }
+    
+    public void removeStudent(Student c){
+        studentList.remove(c);
+    }
+    
+    public boolean checkIfUsernameIsUnique(String email){
+        for (Student e : studentList){
+            if (e.getEmail().equals(email))
+                return false;
+        }
+        return true;
+    }
+    
+    public Student updateStudent(Student cus, String name, Double height, Double weight, String gender, int age, String address, String city, String zipcode){
+        cus.setName(name);
+        cus.setHeight(height);
+        cus.setWeight(weight);
+        cus.setGender(gender);
+        cus.setAge(age);
+        cus.setAddress(address);
+        cus.setCity(city);
+        
+        return cus;
+    } 
+    
 }
+
